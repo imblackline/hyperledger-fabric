83d9064a5e915ac8352def205b47532bef210d76
avolast <49277583+avolast@users.noreply.github.com>
2023-11-13 16:32:16 +0100

    Update interact_as_org1_cli.sh

--- interact_as_org1_cli.sh
+++ interact_as_org1_cli.sh
@@ -12,7 +12,7 @@ peer chaincode invoke -o localhost:7050 --ordererTLSHostnameOverride orderer.exa
 "${PWD}/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem" \
 --peerAddresses localhost:7051 --tlsRootCertFiles ${PWD}/organizations/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/ca.crt \
 --peerAddresses localhost:9051 --tlsRootCertFiles ${PWD}/organizations/peerOrganizations/org2.example.com/peers/peer0.org2.example.com/tls/ca.crt \
--C mychannel -n basic -c '{"function":"myTransaction2","Args":["asset99"]}'
+-C mychannel -n basic -c '{"function":"myTransactionT","Args":["asset99"]}'
 
 # for evaluation transactions
 peer chaincode query -C mychannel -n basic -c '{"function":"myTransactionK","Args":["asset99"]}'
